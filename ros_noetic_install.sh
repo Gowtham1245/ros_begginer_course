a7baf2bdf3f0019e709321fccabe761463c21a61
Gowtham1245 <114413959+Gowtham1245@users.noreply.github.com>
2023-03-26 20:17:17 +0530

    Update ros_noetic_install.sh

--- ros_noetic_install.sh
+++ ros_noetic_install.sh
@@ -1,1 +1,3 @@
+#ROS noetic one line installation
+
 wget -c https://raw.githubusercontent.com/qboticslabs/ros_install_noetic/master/ros_install_noetic.sh && chmod +x ./ros_install_noetic.sh && ./ros_install_noetic.sh
